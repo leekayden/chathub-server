f1bdf924d74dc07117d522cdf72f3f08d9f87de1
leekayden <kayden@programmer.net>
2023-05-20 21:12:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,9 @@ mongoose
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.get("/", (req, res) => {
+  res.send("Server");
+});
 
 const server = app.listen(process.env.PORT || 5000, () =>
   console.log(`Server started on ${process.env.PORT}`)
