58b09b8a8d3faaa0bbd3e464b2b6e1a1c852cecc
leekayden <kayden@programmer.net>
2023-05-21 12:23:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ const morgan = require("morgan");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost:27017/chat"
 
 app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(mongoUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
